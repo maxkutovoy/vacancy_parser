50acf41f27a1ffcedc24377b40a73b4a4771863b
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-26 13:00:05 +0600

    Вывел список вакансий с hh "программист"

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import requests
+
 
 def main():
-    pass
+    hh_api_url = 'https://api.hh.ru/vacancies'
+    params = {
+        "text": "программист"
+    }
+    response = requests.get(hh_api_url, params=params)
+    response.raise_for_status()
+    print(response.url)
+    print(response.json())
 
 
 if __name__ == '__main__':
